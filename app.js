5be6857842423bb4257a0347ee96c37ce1d7c82a
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-09-24 19:35:41 +0400

    get request

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  //   res.send('Hello from server!');
-  //   to specify the status
-  //   by using .json it will automatically set our content-type to application/json
-  res.status(200).json({ message: 'Hello from server!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   //   res.send('Hello from server!');
+//   //   to specify the status
+//   //   by using .json it will automatically set our content-type to application/json
+//   res.status(200).json({ message: 'Hello from server!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('you can post to here');
+// app.post('/', (req, res) => {
+//   res.send('you can post to here');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
