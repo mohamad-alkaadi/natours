774a488e0024aeadfb9bb2daba711337f8208cde
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-09-25 23:09:44 +0400

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// middleware: is a function that can modify the incoming request data
-// it is called middleware because it stands in the middle of the request and the response
-// it is a step that the request goes through while it's being processed
-// the express.json is a middleware
 app.use(express.json());
 
 const tours = JSON.parse(
@@ -22,17 +18,37 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// get with parameters (id for example)
+
+// optional parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  // when we multiply a string that is a number like '3' it will return a number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(200).json({
+      status: 'fail',
+      massage: 'Invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 // post request
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  // Object.assign allows us to create a new object by merging two existing objects together
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      // res.status.json send a response like res.send
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
