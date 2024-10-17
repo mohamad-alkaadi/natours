cd377139fc3b89fccccbda2660aa3ce95efad5a4
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-10-17 23:55:07 +0400

    param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    // if there is no return here express will send the response back but stay in the function and will never continue to next()
+    return res.status(404).json({
+      status: 'fail',
+      massage: 'Invalid id',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,16 +27,11 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  `console`.log(req.params);
+  console.log(req.params);
   // when we multiply a string that is a number like '3' it will return a number
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      massage: 'Invalid id',
-    });
-  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -48,12 +54,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      massage: 'Invalid id',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -63,14 +63,8 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      massage: 'Invalid id',
-    });
-  }
   // we send a response of data null o indicate that the data is no longer there
-  // it returns 204 and dont show anything on postman
+  // it returns 204 and don't show anything on postman
   res.status(204).json({
     status: 'success',
     data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
-
 const router = express.Router();
+// param middleware function
+// it will hold the value of the parameter
+router.param('id', tourController.checkId);
 
 router
   .route('/')
