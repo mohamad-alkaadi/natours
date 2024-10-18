1ba2164d28c51087dadc06da38d43dfe04b07fc1
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-10-18 20:54:54 +0400

    serving static files and having multiple middlewares for the same routes

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const userRouter = require('./routes/userRoutes');
 // 1) Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+// we use express.static middleware in order to serve static files
+// when we use the public folder we use the url http://127.0.0.1:3000/overview.html
+// because when we open up a URL that express can't find in any of our routes it will then look in the public folder that we defined and it sets that folder to the root
+// we don't write public in the url
+app.use(express.static(`${__dirname}/public`));
 // the next argument is always the third after req and res so we can call next what ever we want  like (n, x, etc...) and will still work like next
 // this rule also applies on req and response
 app.use((req, res, next) => {
