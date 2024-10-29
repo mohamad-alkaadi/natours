be94887c54d33bd0d189a43bc0a08658fe518250
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-10-29 20:52:22 +0400

    creating our first schema and model

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 // a good practace to make everything related to express in a file and everything related to the server in another
 // everything that is not related to express we do it outside of app.js
 // we also use database configurations, error handling, or environment variables
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// the connect will return a promise
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+// a model is like a blueprint that we use to create documents
+// we create a model in mongoose in order to create documents using it and also to query, update and delete these documents (CRUD)
+// we create models out of mongoose schema
+// we use a schema to describe our data, to set default values, to validate the data, etc...
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    // schema type options
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // we can also set default values
+
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    // to specify what error will be displayed when we are missing this field
+    // first in the array if required is active or not, second if array is the error that will be displayed
+    // this is called data validation
+    required: [true, 'A true must have a name'],
+  },
+});
+
+// now we create a model out of it
+// it should start with upper case
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
