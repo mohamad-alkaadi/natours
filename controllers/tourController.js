90a7c87abfdae90e4e47e43fbe5d6c4c95a61930
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-11-19 14:55:41 +0400

    AGGREGATION PIPELINE

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -118,3 +118,61 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+// AGGREGATION PIPELINE
+exports.getTourStats = async (req, res) => {
+  try {
+    // its like a query but the difference that we can manipulate data in different steps
+    // we pass the aggregate function an array of the stages
+    // you can find these stages in https://www.mongodb.com/docs/manual/reference/operator/aggregation-pipeline/
+    // you need to await for it to work
+    const stats = await Tour.aggregate([
+      {
+        // the match stage is used to filter the documents
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        // it allows us to group documents together basically using accumulators
+        // we can group for example by difficulty level or by price
+        // we want to group the averages for all the tours in one big group
+        // avg is a keyword
+        $group: {
+          // to group the results using fields we put the name of the field we want to group by in the id
+          // for example we want to group using the rating difficulty level
+          // _id: '$ratingsAverage',
+          // or we can group them by difficulty level
+          // _id: '$difficulty',
+          //also we can convert the result to uppercase
+          _id: { $toUpper: '$difficulty' },
+          // we add one for each documenets using $sum: 1
+          num: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+        // in the sort we need to use the fields the we created in the group stage, we sort the grouped fileds
+      },
+      {
+        $sort: {
+          avgPrice: 1, // 1 is ascending, -1 is descending
+        },
+      },
+      // {
+      //   // we can do stages more than one
+      //$NE ===> NOT EQUAL
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
