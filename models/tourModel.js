40d4d032ad96ce46e47c2eab780a44d18ffac8eb
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-12-02 13:22:44 +0400

    query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -50,44 +50,48 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date], // accepted date format "2021-03-21, 11:32"
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
-    // when the data is being outputted as json
     toJSON: { virtuals: true },
-    // when the data is being outputted as object
     toObject: { virtuals: true },
   }
 );
-// tourSchema.virtual('name of the virtual property'). (the method)
-// when we want to use this we use regular functions
+
 tourSchema.virtual('durationWeeks').get(function () {
-  // 'this" will point to the current document
   return this.duration / 7;
 });
 
-//this is a pre middleware that will run before the save method
-// this is also called a PRE SAVE HOOK
-//DOCUMENT MIDDLEWARE: it runs before .save() and .create() methods but not after .insertMany .find .update .remove .delete .updateMany .updateOne .removeOne .deleteOne
-// the save event is triggered before a document is saved to the database
-// we use a normal function so we can access the this keyword, which is the document that is being processed (saved)
-// we created a empty slug in the schema to hold the slug of the tour
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-//we can have multiple pre event hooks on the same event
-// tourSchema.pre('save', function (next) {
-//   console.log('will save document...');
-//   next();
-// });
-// post middleware : it runs after the save method
-//this is called a POST SAVE HOOK
-// it has access to next and the document that was just saved to the database
-// tourSchema.post('save', function (doc, next) {
-//   console.log(doc);
-//   next();
-// });
+// QUERY MIDDLEWARE
+// the 'find' parameter will make it a query middleware not a document middleware
+// the 'find' parameter means we wand to run this middleware before the 'find' query
+// example the we want to make secret tour in our database for very small vip group of people
+// and we don't want our secret tours to appear in the result of our public route
+//so we want to create a secret tour field and then query only for tours that are not secret
+// tourSchema.pre('find', function (next) {
+// if we want to make it work on all of the find queries: find, findOne, findMany, findById ... we use regular expressions
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+// in the post find middleware we have access to all the documents that were found in our query
+// this wil run post the find method
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+
+  console.log(docs);
+
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
