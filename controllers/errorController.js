332e99d041da64ddad3cb0aa730d8c6780b0b7e5
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-12-24 01:37:16 +0400

    errors in development vs production and operational vs non operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // programming or other unknown error: don't leak details to the client
+  } else {
+    // 1) log error
+    console.error('Error :(', err);
+    // 2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // we define the default status code
   // this means if express knows the error it will provide it to us if not return 500
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
