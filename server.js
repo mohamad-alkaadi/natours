f68a654c88ab98cd949b1aed86fc60dd7ea988ac
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2025-01-01 14:13:12 +0400

    Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running ono port ${port}`);
+});
+
+// this is called a saifty net
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  //to exit our application // we pass it a code // 0 is passed for success // 1 is passed for uncaught exception
+  console.log('UNHANDLED REJECTION! 💥 shutting down...');
+  // on server.close we give the server time to finish all the request that are still pending or being handled at the time and after that the server is killed
+  server.close(() => {
+    process.exit(1);
+  });
 });
